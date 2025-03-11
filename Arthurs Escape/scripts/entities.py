d26038639ec134c909b6c4ce58dfef7dd6eacadf
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-03-11 10:56:17 -0300

    Implementando Jogador e cenario

--- Arthurs Escape/scripts/entities.py
+++ Arthurs Escape/scripts/entities.py
@@ -2,15 +2,19 @@ import sys
 import pygame
 
 class PhysicsEntity:
-    def __init__(self, game):
+    def __init__(self, game, e_type, pos, size):
         self.game = game
-        self.game.entities.append(self)
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = [0, 0]
 
-        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
-        self.img.set_colorkey((0, 0, 0))
+    def update(self, movement=(0, 0)):
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
-        self.img_pos = [160, 260]
-        self.movement = [False, False]
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
 
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+    def render(self, surf):
+        surf.blit(self.game.assets['player'], self.pos)
         
