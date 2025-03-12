86b6565ece221beb99c3b013308d2bb1b7db2084
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-03-12 10:35:35 -0300

    Física e colisões

--- Arthurs Escape/game.py
+++ Arthurs Escape/game.py
@@ -10,7 +10,7 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('ninja game')
+        pygame.display.set_caption("Arthur's Escape")
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
