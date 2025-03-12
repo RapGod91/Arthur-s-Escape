c5ca2e254dd6b545895a2b78879097e88b2eedf2
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-03-12 10:34:21 -0300

    Física e colisões

--- Arthurs Escape/game.py
+++ Arthurs Escape/game.py
@@ -4,40 +4,41 @@ import pygame
 
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption("Arthur's Escapes")
-        self.screen = pygame.display.set_mode((800,600))
-
+        pygame.display.set_caption('ninja game')
+        self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
-
+        
         self.movement = [False, False]
-
+        
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            "player": load_image("entities/player.png")
+            'player': load_image('entities/player.png')
         }
-
-        print(self.assets)
-
-        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
-
+        
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        
+        self.tilemap = Tilemap(self, tile_size=16)
+        
     def run(self):
-
         while True:
             self.display.fill((14, 219, 248))
-
-            self.player.update((self.movement[1] - self.movement[0], 0))
+            
+            self.tilemap.render(self.display)
+            
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
-
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -47,6 +48,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
--- Arthurs Escape/scripts/utils.py
+++ Arthurs Escape/scripts/utils.py
@@ -13,4 +13,4 @@ def load_images(path):
     images = []
     for img_name in os.listdir(BASE_IMG_PATH + path):
         images.append(load_image(path + '/' + img_name))
-        return images
+    return images
