0f79ddb05aa678eaa74f254598924ddeb6fb2ed9
RapGod91 <raphaelrodriguessilva@gmail.com>
2025-03-28 10:57:58 -0300

    Implementação de inimigos, armas e morte

--- Arthurs Escape/editor.py
+++ Arthurs Escape/editor.py
@@ -11,7 +11,7 @@ class Editor:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption("editor")
+        pygame.display.set_caption('editor')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
@@ -22,23 +22,24 @@ class Editor:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
+            'spawners': load_images('tiles/spawners'),
         }
         
         self.movement = [False, False, False, False]
-              
+        
         self.tilemap = Tilemap(self, tile_size=16)
-
+        
         try:
             self.tilemap.load('map.json')
-        except:
+        except FileNotFoundError:
             pass
-
+        
         self.scroll = [0, 0]
-
+        
         self.tile_list = list(self.assets)
         self.tile_group = 0
         self.tile_variant = 0
-
+        
         self.clicking = False
         self.right_clicking = False
         self.shift = False
@@ -47,22 +48,22 @@ class Editor:
     def run(self):
         while True:
             self.display.fill((0, 0, 0))
-
+            
             self.scroll[0] += (self.movement[1] - self.movement[0]) * 2
             self.scroll[1] += (self.movement[3] - self.movement[2]) * 2
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
-
+            
             self.tilemap.render(self.display, offset=render_scroll)
-
+            
             current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
             current_tile_img.set_alpha(100)
-
+            
             mpos = pygame.mouse.get_pos()
             mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
             tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
-
+            
             if self.ongrid:
-                self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - render_scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
+                self.display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
             else:
                 self.display.blit(current_tile_img, mpos)
             
@@ -77,13 +78,14 @@ class Editor:
                     tile_r = pygame.Rect(tile['pos'][0] - self.scroll[0], tile['pos'][1] - self.scroll[1], tile_img.get_width(), tile_img.get_height())
                     if tile_r.collidepoint(mpos):
                         self.tilemap.offgrid_tiles.remove(tile)
-
+            
             self.display.blit(current_tile_img, (5, 5))
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                    
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.clicking = True
@@ -108,6 +110,7 @@ class Editor:
                         self.clicking = False
                     if event.button == 3:
                         self.right_clicking = False
+                        
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_a:
                         self.movement[0] = True
